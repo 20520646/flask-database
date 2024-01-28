0b38b0b8eec6dba343bf478841c36f625f1d3392
20520646 <20520646@gm.uit.edu.vn>
2024-01-28 11:37:18 +0700

    sửa file

--- api/face_classification.py
+++ api/face_classification.py
@@ -8,50 +8,60 @@ from extract_faces import extract_face
 from make_embeddings import get_embeddings
 import pickle
 from firebase1 import get_firebase
-def train_model(faces, labels):
+def train_model(train_faces, train_labels, test_faces, test_labels):
    
     in_encoder = Normalizer(norm ='l2')
-    faces = in_encoder.transform(faces)
+    train_faces = in_encoder.transform(train_faces)
+    test_faces = in_encoder.transform(test_faces)
+    print('Dataset: train=%d, test=%d' % (train_faces.shape[0], test_faces.shape[0]))
+
     out_encoder = LabelEncoder()
-    out_encoder.fit(labels)
+    out_encoder.fit(train_labels)
 
     # print(labels, type(labels))
 
-    labels = out_encoder.transform(labels)
+    train_labels = out_encoder.transform(train_labels)
+    test_labels = out_encoder.transform(test_labels)
+
     # print(labels)
 
     model = SVC(kernel='linear', probability=True)
-    model.fit(faces, labels)
+    model.fit(train_faces, train_labels)
 
-    yhat_train = model.predict(faces)
-    
-    score_train = accuracy_score(labels, yhat_train)
+    yhat_train = model.predict(train_faces)
+    yhat_test = model.predict(test_faces)
+    score_train = accuracy_score(train_labels, yhat_train)
+    score_test = accuracy_score(test_labels, yhat_test)
+
     # print(labels, yhat_train)
-    print(f'>>> Accuracy: {score_train*100}')
+    print(f'>>> Accuracy train: {score_train*100}')
+    print(f'>>> Accuracy test: {score_test*100}')
     
     return model, out_encoder
 
 if __name__ == '__main__':
     url = './data_embeddings.npz'
     data = load(url)
-    faces, labels = data['arr_0'], data['arr_1']
+    train_faces, train_labels, test_faces, test_labels = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
     # print(f'>>> Dataset: train={faces.shape[0]}')
-    # model, out_encoder = train_model(faces, labels)
+    model, out_encoder = train_model(train_faces, train_labels, test_faces, test_labels)
     # in_encoder = Normalizer(norm ='l2')
     # faces = in_encoder.transform(faces)
-    out_encoder = LabelEncoder()
-    out_encoder.fit(labels)
+    # out_encoder = LabelEncoder()
+    # out_encoder.fit(train_labels)
 
     # print(labels, type(labels))
 
-    labels = out_encoder.transform(labels)
-    with open('faces_classification.pkl',  'rb') as file:
-       model = pickle.load(file)
-    # with open('faces_classification.pkl',  'wb') as file:
-    #     pickle.dump(model,file)
-    print(labels)
-    img = get_firebase()
-    face, box = extract_face("",link = False, img = img)
+    train_labels = out_encoder.transform(train_labels)
+    # with open('faces_classification.pkl',  'rb') as file:
+    #    model = pickle.load(file)
+    with open('faces_classification.pkl',  'wb') as file:
+        pickle.dump(model,file)
+    print(train_labels)
+    # img = get_firebase()
+    # face, box = extract_face("",link = False, img = img)
+    img = '/home/kltnofpandn/Desktop/flask-database/dataset/test/20520646/45.jpg'
+    face, box = extract_face(img)
     embedding = get_embeddings([face])
 
     yhat_class = model.predict(embedding)
@@ -63,3 +73,5 @@ if __name__ == '__main__':
     print("probability: ", class_probability)
     print("name: ", predict_name[0])
 
+#port ip public 42.116.6.42:8276 8287
+    #ip nooij bo 192.168.4.170
--- api/make_embeddings.py
+++ api/make_embeddings.py
@@ -11,14 +11,15 @@ def get_embeddings(faces):
 #make a file contains embeding vectors
 def makes_embeddings_file(url):
     data = load(url)
-    faces, labels = data['arr_0'], data['arr_1']
-    embeddings = get_embeddings(faces)
-    print(">>> Train shape: ", embeddings.shape)
+    train_faces, train_labels, test_faces, test_labels = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3'],
+    train_embeddings = get_embeddings(train_faces)
+    print(">>> Train shape: ", train_embeddings.shape)
+    test_embeddings = get_embeddings(test_faces)
+    print(">>> Test shape: ", test_embeddings.shape)
     file_name = 'data_embeddings.npz'
-    savez_compressed(file_name, embeddings, labels)
+    savez_compressed(file_name, train_embeddings, train_labels, test_embeddings, test_labels)
     
 if __name__ == '__main__':
     url = './data_faces.npz'
     makes_embeddings_file(url)
-    
     
