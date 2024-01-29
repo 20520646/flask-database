86d92999f40bc44815e6b803cde4adba429ae241
phatnt472 <20521737@gm.uit.edu.vn>
2024-01-29 20:20:06 +0700

    Update chupanh.py

--- chupanh.py
+++ chupanh.py
@@ -3,6 +3,7 @@ from time import sleep
 count = 51
 cap = cv2.VideoCapture(0)
 limit = 100
+name = "Nam"
 while  count <= limit:
     ret, frame =  cap.read()
     key = cv2.waitKey(10)
@@ -15,10 +16,10 @@ while  count <= limit:
     # print("1")
     if not ret or key == ord('q'):
         break
-    cv2.imwrite(f'train/Nam/{count}.jpg', frame)
+    cv2.imwrite(f'train/{name}/{count}.jpg', frame)
     sleep(1)
     cv2.imshow(f'{count}',frame)
     print(count)
     count += 1
-cv2.destroyAllWindows()
+cv2.destroyAllWin()
 cap.release()
